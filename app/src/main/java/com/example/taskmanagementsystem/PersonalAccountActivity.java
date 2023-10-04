71356c83cba032c48788cf208c07e9c341fbfd02
akkafe1ix <alex.from2018@yandex.ru>
2023-10-05 00:23:48 +0300

    GUI Account/Profile

--- app/src/main/java/com/example/taskmanagementsystem/PersonalAccountActivity.java
+++ app/src/main/java/com/example/taskmanagementsystem/PersonalAccountActivity.java
@@ -2,13 +2,47 @@ package com.example.taskmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PersonalAccountActivity extends AppCompatActivity {
+
+    public boolean EditBool= true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_account);
+    }
+    public void StartEditProfile(View view){
+
+        TextView SurnameText = findViewById(R.id.editTextText3);
+        TextView NameText = findViewById(R.id.editTextText6);
+        TextView OrganizationText = findViewById(R.id.editTextText8);
+        TextView PostText = findViewById(R.id.editTextText9);
+        Button ButEditSave= findViewById(R.id.button2);
+        SurnameText.setEnabled(EditBool);
+        SurnameText.setFocusable(EditBool);
+        NameText.setEnabled(EditBool);
+        NameText.setFocusable(EditBool);
+        OrganizationText.setEnabled(EditBool);
+        OrganizationText.setFocusable(EditBool);
+        PostText.setEnabled(EditBool);
+        PostText.setFocusable(EditBool);
+        if (EditBool==false){
+            EditBool=true;
+            ButEditSave.setText("Редактировать");
+        } else{
+            EditBool=false;
+            ButEditSave.setText("Сохранить");
+        }
+    }
+
+    public void ExitInAccount(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
