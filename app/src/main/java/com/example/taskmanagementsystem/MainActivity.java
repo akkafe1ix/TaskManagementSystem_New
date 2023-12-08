8e4dd9b0a6f026cd90c9a16adaf77ff8293b2871
akkafe1ix <alex.from2018@yandex.ru>
2023-12-09 00:44:58 +0300

    Added вход

--- app/src/main/java/com/example/taskmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/taskmanagementsystem/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.taskmanagementsystem;
 
 
 
+import static java.lang.Boolean.TRUE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,16 +12,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView responseTextView;
     private String host = "82.179.140.18";
     private int port = 45683;
-    // Создание объекта SocketManager
-    //SocketManager socketManager;
+    public static String idClient="";
+    public static String name="";
+    public static String surname="";
+    public static String organization="";
+    public static String post="";
     boolean isConnected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +37,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SocketManager.connectParallel(host,port);
-        //SocketManager.sendParallel("Select ")
-        //boolean isConnected = SocketManager.getConnect();
     }
 
     public void SingInAccount(View v){
+        EditText login = (EditText) findViewById(R.id.editTextText);
+        EditText passwd = (EditText) findViewById(R.id.editTextTextPassword);
+        SocketManager.sendParallel("Select * from accounts where login='"+login.getText()+"' and passwd='"+passwd.getText()+"'");
+        SocketManager.receiveParallel();
+        String buf=SocketManager.getResult();
+        if(!buf.equals(" ")) {
+            String[] ClientInfo = buf.split("\t");
+            idClient = ClientInfo[0];
+            name = ClientInfo[3];
+            surname = ClientInfo[4];
+            organization = ClientInfo[5];
+            post = ClientInfo[6];
+            Intent intent = new Intent(this, PersonalAccountActivity.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(this, "Неверный логин или пароль!", Toast.LENGTH_SHORT).show();
+        }
 
-        Intent intent = new Intent(this,PersonalAccountActivity.class);
-        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/taskmanagementsystem/PersonalAccountActivity.java
+++ app/src/main/java/com/example/taskmanagementsystem/PersonalAccountActivity.java
@@ -16,6 +16,15 @@ public class PersonalAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_account);
+        TextView SurnameText = findViewById(R.id.editTextText3);
+        SurnameText.setText(MainActivity.surname);
+        TextView NameText = findViewById(R.id.editTextText6);
+        NameText.setText(MainActivity.name);
+        TextView OrganizationText = findViewById(R.id.editTextText8);
+        OrganizationText.setText(MainActivity.organization);
+        TextView PostText = findViewById(R.id.editTextText9);
+        PostText.setText(MainActivity.post);
+
     }
     public void StartEditProfile(View view){
 
