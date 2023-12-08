fd3e846389ab865c622324c1a33fd21a9ceaa152
akkafe1ix <alex.from2018@yandex.ru>
2023-12-08 23:02:37 +0300

    Added con with server

--- app/src/main/java/com/example/taskmanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/taskmanagementsystem/MainActivity.java
@@ -1,20 +1,38 @@
 package com.example.taskmanagementsystem;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView responseTextView;
+    private String host = "82.179.140.18";
+    private int port = 45683;
+    // Создание объекта SocketManager
+    //SocketManager socketManager;
+    boolean isConnected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SocketManager.connectParallel(host,port);
+        //SocketManager.sendParallel("Select ")
+        //boolean isConnected = SocketManager.getConnect();
     }
 
     public void SingInAccount(View v){
+
         Intent intent = new Intent(this,PersonalAccountActivity.class);
         startActivity(intent);
     }
---
+++ app/src/main/java/com/example/taskmanagementsystem/SocketManager.java
@@ -0,0 +1,129 @@
+package com.example.taskmanagementsystem;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SocketManager {
+    private static Socket socket;
+    private static InputStream inputStream;
+    private static PrintWriter printWriter;
+    private static boolean connected = false;
+    private static String result;
+
+    public static void connect(String host, int port) {
+        if (!connected) {
+            try {
+                socket = new Socket(host, port);
+                inputStream = socket.getInputStream();
+                printWriter = new PrintWriter(socket.getOutputStream(), true);
+                connected = true;
+            } catch (IOException exception) {
+
+            }
+        }
+    }
+
+    public static boolean connectParallel(String host, int port) {
+        Thread thread = new Thread(() -> {
+            connect(host, port);
+            receive();
+        });
+        thread.start();
+
+        try {
+            thread.join(1000);
+        } catch (InterruptedException exception) {
+            thread.interrupt();
+        }
+
+        return connected;
+    }
+
+    public static void disconnect() {
+        if (connected) {
+            try {
+                socket.close();
+                inputStream.close();
+                printWriter.close();
+                connected = false;
+            } catch (IOException ignored) {
+
+            }
+        }
+    }
+
+    public static boolean disconnectParallel() {
+        Thread thread = new Thread(SocketManager::disconnect);
+        thread.start();
+
+        try {
+            thread.join(1000);
+            return true;
+        } catch (InterruptedException exception) {
+            thread.interrupt();
+            return false;
+        }
+    }
+
+    public static void send(String message) {
+        if (connected) {
+            printWriter.println(message);
+        }
+    }
+
+    public static boolean sendParallel(String message) {
+        if (connected) {
+            Thread thread = new Thread(() -> {
+                send(message);
+            });
+            thread.start();
+
+            try {
+                thread.join(1000);
+                return true;
+            } catch (InterruptedException exception) {
+                thread.interrupt();
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static void receive() {
+        if (connected) {
+            try {
+                byte[] buffer = new byte[1024];
+                int bytesRead = inputStream.read(buffer);
+                result = new String(buffer, 0, bytesRead);
+            } catch (IOException ignored) {
+
+            }
+        }
+    }
+
+    public static boolean receiveParallel() {
+        if (connected) {
+            Thread thread = new Thread(SocketManager::receive);
+            thread.start();
+
+            try {
+                thread.join(1000);
+                return true;
+            } catch (InterruptedException exception) {
+                thread.interrupt();
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public static String getResult() {
+        return result;
+    }
+
+    public static Boolean getConnect(){return  connected;}
+}
